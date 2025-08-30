decb0d4f74c770e1c4cd8538c2e2d4b765411f3b
Flavio <lanbinhohouse@gmail.com>
2025-08-30 13:25:15 -0300

    fix: concertando o chat

--- src/app/components/Socket/index.tsx
+++ src/app/components/Socket/index.tsx
@@ -1,12 +1,11 @@
 import { io } from "socket.io-client";
 
-const BACKEND_URL =
-  process.env.NODE_ENV === "production"
-    ? process.env.NEXT_PUBLIC_API_RENDER
-    : process.env.NEXT_PUBLIC_API_URL;
+const BACKEND_URL = process.env.NEXT_PUBLIC_API_RENDER || "https://backend-lanchonete.onrender.com";
 
 const socket = io(BACKEND_URL, {
   transports: ["websocket", "polling"],
 });
 
 export default socket;
+
+//https://backend-lanchonete.onrender.com
